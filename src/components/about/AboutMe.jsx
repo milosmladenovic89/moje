718e3e23c4e28ddaf9b23fc0c8e3a9c6fe327267
milosmladenovic89 <milos.mladenovic@foreach.rs>
2023-07-08 20:52:55 +0200

    added hr animation and adjusted rest animation timing

--- src/components/about/AboutMe.jsx
+++ src/components/about/AboutMe.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import { motion } from 'framer-motion';
+import '../../styles/AboutMe.css'
+
 
 export default function AboutMe() {
   return (
-    <div>AboutMe</div>
-  )
+    <motion.div
+    className="div-about-container"
+    initial={{ y: -1000 }}
+    animate={{ y: 0 }}
+    transition={{ duration: 0.5 }}
+  >
+    <div className="link-nav-container">
+      <div></div>
+      <Link className="link-nav" to="/">HOME</Link>
+    </div>
+
+    <h1>ABOUT PAGE</h1>
+    <div className="noise"></div>
+  </motion.div>
+)
+  
 }
--- src/components/homePage/Home.jsx
+++ src/components/homePage/Home.jsx
@@ -1,7 +1,308 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react'
+import { motion, useInView } from 'framer-motion'
+import { Link } from 'react-router-dom'
+import '../../styles/Home.css'
 
-export default function Home() {
+
+function SectionText({ children }) {
+  const ref = useRef(null)
+  const isInView = useInView(ref, { once: true })
   return (
-    <div>Home</div>
+    <section ref={ref} className="anime2">
+      <span
+        style={{
+          lineHeight: isInView ? 1 : 0,
+          transition: 'all 1s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s',
+        }}
+      >
+        {children}
+      </span>
+    </section>
+  )
+}
+
+function SectionText3({ children }) {
+  const ref = useRef(null)
+  const isInView = useInView(ref, { once: true })
+  return (
+    <section ref={ref} className="anime22">
+      <span
+        style={{
+          lineHeight: isInView ? 1 : 0,
+          transition: 'all 1s cubic-bezier(0.17, 0.55, 0.55, 1) 1.8s',
+        }}
+      >
+        {children}
+      </span>
+    </section>
+  )
+}
+function SectionText2({ children }) {
+  const ref = useRef(null)
+  const isInView = useInView(ref, { once: true })
+  return (
+    <section className="anime2" ref={ref}>
+      <span
+        style={{
+          opacity: isInView ? 1 : 0,
+          transform: isInView ? 'translateX(0)' : 'translateX(1000px)',
+          transition: 'all 1s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s',
+        }}
+      >
+        {children}
+      </span>
+    </section>
+  )
+}
+
+function SectionImage({ children }) {
+  const ref = useRef(null)
+  const isInView = useInView(ref, { once: true })
+  return (
+    <section ref={ref} className="animeImage">
+      <span
+        style={{
+          transform: isInView ? 'scale(1)' : 'scale(0)',
+          transition: 'all 1s cubic-bezier(0.17, 0.55, 0.55, 1) 1s',
+        }}
+      >
+        {children}
+      </span>
+    </section>
+  )
+}
+
+function SectionImg2({ children }) {
+
+  const [mobile, setMobile] = useState(window.innerWidth)
+
+  useEffect(() => {
+    const mobileWidth = () => {
+      setMobile(window.innerWidth)
+    }
+    window.addEventListener('resize', mobileWidth)
+
+    return () => {
+      window.removeEventListener('resize', mobileWidth)
+    }
+  }, [])
+
+  const ref = useRef(null)
+  const isInView = useInView(ref, { once: true })
+  return (
+    <section ref={ref} className="animeImage2">
+      <span
+        style={{
+          transform: 'translate(-50%, -50%)',
+          top: '50%',
+          left: '50%',
+          width:
+            mobile > 600
+              ? isInView
+                ? '50vw'
+                : '10vw'
+              : mobile < 600 && isInView
+                ? '80vw'
+                : '30vw',
+          height:
+            mobile > 600
+              ? isInView
+                ? '50vh'
+                : '40vh'
+              : mobile < 600 && isInView
+                ? '20vh'
+                : '10vh',
+          zIndex: 999,
+          transition: 'all 1s cubic-bezier(0.17, 0.55, 0.55, 1) 0.8s',
+        }}
+      >
+        {children}
+      </span>
+    </section>
+  )
+}
+
+function SectionHr({ children }) {
+  const ref = useRef(null)
+  const isInView = useInView(ref, { once: true })
+  return (
+    <section className='animeHr' ref={ref}>
+      <span
+        style={{
+
+          transformOrigin: "0%",
+          width: isInView ? '100%' : '0%',
+          transition: 'all 1.4s cubic-bezier(0.17, 0.55, 0.55, 1) 0.4s',
+        }}
+      >
+        {children}
+      </span>
+    </section>
+  )
+}
+export default function Homepage() {
+  let text = 'Milos Mladenovic - Frontend Developer'.split('')
+
+  const [mobile, setMobile] = useState(window.innerWidth)
+  const [img, setImg] = useState('bg-1')
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (img === "bg-1") {
+        setImg("bg-2")
+      } else if (img === 'bg-2') {
+        setImg('bg-3')
+      } else if (img === 'bg-3') {
+        setImg("bg-1")
+      }
+    }, 5000);
+
+    return () => {
+      clearInterval(interval)
+    }
+  }, [img])
+
+  useEffect(() => {
+    const mobileWidth = () => {
+      setMobile(window.innerWidth)
+    }
+    window.addEventListener('resize', mobileWidth)
+
+    return () => {
+      window.removeEventListener('resize', mobileWidth)
+    }
+  }, [])
+
+
+  return (
+    <div className="main">
+      <div>
+        <motion.div
+          initial={{ y: 1000 }}
+          animate={{ y: 0 }}
+          transition={{ duration: 0.5 }}
+          className="after"
+        >
+          <div className="nav-container">
+            <div></div>
+
+            <div>
+              <span>
+                <Link className="linkNav" to="/about">
+                  About
+                </Link>
+              </span>
+              <span style={{ marginLeft: '100px' }}>
+                <Link className="linkNav" to="/">
+                  Home
+                </Link>
+              </span>
+            </div>
+          </div>
+
+          <div className="heading">
+            <div className="fonts">
+              <SectionText>
+                <div style={{ position: 'absolute', top: '20%' }}>
+                  FREELANCE
+                </div>
+              </SectionText>
+              <SectionText>
+                <div style={{ position: 'absolute', top: '30%' }}>FRONTEND</div>
+              </SectionText>
+              <SectionText>
+                <div style={{ position: 'absolute', top: '40%' }}>
+                  DEVELOPER
+                </div>
+              </SectionText>
+              <SectionText>
+                <div
+                  className="text-transparent"
+                  style={{ position: 'absolute', top: '50%' }}
+                >
+                  BASED IN{' '}
+                </div>
+              </SectionText>
+              <SectionText>
+                <div
+                  className="text-transparent"
+                  style={{ position: 'absolute', top: '60%' }}
+                >
+                  SERBIA
+                </div>
+              </SectionText>
+            </div>
+
+            <SectionImage>
+              <div className="circle-container">
+                <div className="circle">
+                  <p className="circleText">
+                    {text.map((item, index) => (
+                      <span
+                        style={{ transform: `rotate(${index * 9.6}deg)` }}
+                        key={index}
+                      >
+                        {item}
+                      </span>
+                    ))}
+                  </p>
+                  <div className={img}></div>
+                </div>
+              </div>
+            </SectionImage>
+          </div>
+
+          <div className='hr-container'>
+            <SectionHr>
+              <hr className='hr' />
+            </SectionHr>
+          </div>
+
+
+          <div className="img-cont-main">
+
+
+
+            <div className="img-container">
+              <SectionImg2></SectionImg2>
+            </div>
+
+            {mobile > 600 && (
+              <SectionText3>
+                <h2 className="div-desktop-text">HELLO WORLD 2</h2>
+                {mobile > 600 && (
+                  <SectionText3>
+                    <h2 className="div-desktop-text">HELLO WORLD 3</h2>
+                  </SectionText3>
+                )}
+              </SectionText3>
+            )}
+
+          </div>
+
+          {mobile < 600 && (
+            <SectionText3>
+              <h2 className="div-mobile-text">HELLO WORLD 2</h2>
+              {mobile < 600 && (
+                <SectionText3>
+                  <h2 className="div-mobile-text">HELLO WORLD 3</h2>
+                </SectionText3>
+              )}
+            </SectionText3>
+          )}
+
+
+
+
+          <div style={{ marginTop: '50vh' }}>
+            <SectionText2>
+              <h2>HELLO WORLD 3</h2>
+            </SectionText2>
+          </div>
+        </motion.div>
+      </div>
+
+      <div className="noise"></div>
+    </div>
   )
 }
